22ea234324306e46ebd67b995aeb3a8f2d1ec778
Ardikaoktavian <ardikaardika17436@gmail.com>
2025-04-17 14:02:35 +0700

    Membuat data dummy books pada Utils dan melakukan mapping

--- latihan-react/src/App.jsx
+++ latihan-react/src/App.jsx
@@ -1,101 +1,24 @@
-import './App.css'
-
-/**
- * Membuat component Header
- * Component Header menampilkan navigasi
- */
-function Header() {
-  return (
-    <nav>
-      <ul>
-        <li>Home</li>
-        <li>About</li>
-        <li>Contact</li>
-      </ul>
-    </nav>
-  )
-}
-
-
-/**
- * Membuat component Content
- * Component Content menampung konten utama
- */
-function Content() {
-  return (
-    <>
-      <h1>Ini adalah konten utama</h1>
-    </>
-  )
-}
-
-
-/**
- * Membuat component Footer
- * Component Footer menampilkan informasi footer
- */
-function Footer() {
-  return (
-    <>
-      <h2>NF Academy</h2>
-      <p>Created by React JS</p>
-    </>
-  )
-}
-
-
-/**
- * Contoh penggunaaan JSX
- *  Menggunakan kurung kurawal {}
- */
-function Hello() {
-  const nama = "Fikri"
-  const umur = 22
-  return (
-    <>
-      <h2>Hello React</h2>
-      <p>Saya {nama} seorang Frontend Developer yang berusia {umur}</p>
-    </>
-  )
-}
-
-
-/**
- * Contoh penggunaaan props
- *  Menggunakan Greeting
- */
-function Greeting(props) {
-  return ( <p>Hello, {props.name}</p>  // Karena hanya satu tag p maka bisa dijadikan satu baris
-  )
-}
-
-
-/**
- * Contoh penggunaaan props
- *  Menggunakan beberapa tag untuk profile
- */
-function Profile(props) {
-  return ( 
-    <>  
-    <h4>Hai, Saya {props.name}</h4>
-    <p>Umur saya {props.age}</p>
-    <p>Saya dari {props.country}</p>
-    </>
-  )
-}
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./Pages";
+import Books from "./Pages/books";
+import Login from "./Pages/auth/login";
+import Register from "./Pages/auth/register";
 
 function App() {
-
   return (
     <>
-      <Header />
-      <Content />
-      <Hello />
-      <Greeting name="Choco" />
-      <Profile name="Joya" age={22} country="Indonesia" />
-      <Footer />
+      <div className="container">
+        <BrowserRouter>
+          <Routes>
+            <Route index element={<Home />} />
+            <Route path="books" element={<Books />} />
+            <Route path="login" element={<Login />} />
+            <Route path="register" element={<Register />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
